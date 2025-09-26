768d374be84c23c880c8e5b42bdec69dbaeffc71
Scarnile <198330506+Scarnile@users.noreply.github.com>
2025-09-26 11:37:25 +0800

    Add menu to remove Heading with right click

--- headingSelectorView.ts
+++ headingSelectorView.ts
@@ -1,5 +1,5 @@
 import HeadingTransporterPlugin, { HeadingTransporterSettings } from "main";
-import { ItemView, Setting, WorkspaceLeaf } from "obsidian"
+import { ItemView, Menu, Notice, Setting, WorkspaceLeaf } from "obsidian"
 
 export const HEADING_SELECTOR_VIEW_TYPE = 'heading-selector-view'
 
@@ -39,6 +39,7 @@ export class HeadingSelectorView extends ItemView {
         container.createEl('h2', { text: 'Heading Selector'});
         
         for (let index = 0; index < this.settings.headingInfos.length; index++) {
+            
             const headingContainer = container.createEl('div', {cls: "hsp-heading-container"})
             headingContainer.createEl('h6', { text: this.settings.headingInfos[index].headingName,
                 cls: "hsp-heading"});
@@ -59,6 +60,22 @@ export class HeadingSelectorView extends ItemView {
                 this.plugin.saveSettings()
                 console.log(this.settings.selectedHeadingIndex)
                 this.display()
+            })
+
+            headingContainer.addEventListener("contextmenu", (event) => {
+                const menu = new Menu()
+                menu.addItem((item) => {
+                    item
+                        .setTitle('Remove')
+                        .setIcon('trash')
+                        .onClick(() => {
+                            console.log(this.settings.headingInfos[index])
+                            this.settings.headingInfos.splice(index)
+                            this.display()
+                        })
+                }) 
+
+                menu.showAtMouseEvent(event)
             })
         }
     }
